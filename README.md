441dc83aebad70ba36c729eeb4c690dc3a7f017e
bsavanth <bsavanth@uark.edu>
2023-12-24 22:47:59 -0600

    Added Spring Security

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Product_Management_Kubernetes
 
 This backend service exposes API's to React clone for product management
+
+Spring swagger docs - http://localhost:8080/swagger-ui/index.html
---
+++ src/main/java/kubecluster/productmanagementbackend/SecurityConfiguration/SecurityConfig.java
@@ -0,0 +1,74 @@
+package kubecluster.productmanagementbackend.SecurityConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/product/createProduct",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder)
+    {
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+        UserDetails user = User.withUsername("savanth").password(passwordEncoder.encode("savanth")).roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+           http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->  auth.requestMatchers("/product/createProduct").permitAll()
+
+                                                .requestMatchers(AUTH_WHITELIST).permitAll()
+                                                .requestMatchers("product/getProducts")
+                                                .authenticated()
+                                      ).formLogin((formLogin) ->
+                           formLogin
+                                   .usernameParameter("admin")
+                                   .passwordParameter("admin"));
+
+
+
+                return http.build();
+
+    }
+
+        @Bean
+        public PasswordEncoder passwordEncoder()
+        {
+            return new BCryptPasswordEncoder();
+        }
+
+
+
+}
